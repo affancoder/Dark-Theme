aa2db11ae9174458c4ca4f2c30665e0d73ec0c82
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2023-08-15 20:48:15 +0530

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// const themeToggle = document.getElementById('theme-toggle');
-// themeToggle.addEventListener('change', () => {
-//   document.body.classList.toggle('dark-theme', themeToggle.checked);
-// });
