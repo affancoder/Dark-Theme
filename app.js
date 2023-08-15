6a4666c5afb76483fa21747250b1e564ccfcc2c1
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2023-08-15 20:47:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// const themeToggle = document.getElementById('theme-toggle');
+// themeToggle.addEventListener('change', () => {
+//   document.body.classList.toggle('dark-theme', themeToggle.checked);
+// });
